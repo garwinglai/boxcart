ccfa414e1bab3c29322c68591d253a1f1e9d85b9
Garwing Lai <garwinglai@gmail.com>
2023-06-23 11:28:11 -0700

    fixed deployment error

--- components/common/loaders/PageLoader.js
+++ components/common/loaders/PageLoader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../styles/components/loaders/page-loader.module.css";
+import styles from "@/styles/components/loaders/page-loader.module.css";
 
 function PageLoader() {
 	return (
