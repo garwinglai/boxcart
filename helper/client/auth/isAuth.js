3f10f97d498038a6b2ae0acc9ceba31bc8c16d08
Garwing Lai <garwinglai@gmail.com>
2023-06-23 11:36:47 -0700

    operating on new computer git clone, updated isAuth route

--- helper/client/auth/isAuth.js
+++ helper/client/auth/isAuth.js
@@ -10,7 +10,7 @@ export async function isAuth(context, next) {
 			redirect: {
 				destination:
 					process.env.NODE_ENV && process.env.NODE_ENV === "production"
-						? "app.boxcart.shop/auth/signin"
+						? "/auth/signin"
 						: "http://app.localhost:3000/auth/signin",
 				permanent: false,
 			},
