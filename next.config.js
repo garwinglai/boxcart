b00c51dc4605f0d75bb4375fd39a123d45fbbf40
Garwing <garwinglai@gmail.com>
2024-01-03 21:11:37 -0800

    Move manifest.json and configure next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const withCopyWebpackPlugin = require("next-copy-webpack-plugin");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withCopyWebpackPlugin({
   images: {
     remotePatterns: [
       {
@@ -30,6 +32,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-};
+});
 
 module.exports = nextConfig;
