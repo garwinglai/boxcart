24ce6d59bb94437788adcbf4b45f928946f0b31d
Garwing <garwinglai@gmail.com>
2024-01-07 12:52:09 -0800

    updated cors in get-products

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -5,14 +5,26 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (tab.url && tab.url.includes("google.com/search")) {
     const queryParameters = tab.url.split("?")[1];
     const urlParameters = new URLSearchParams(queryParameters);
-
+    const query = urlParameters.get("q");
     if (changeInfo.status === "complete") {
+      const products = await fetchProducts(query);
       chrome.tabs.sendMessage(tabId, {
         type: "NEW_SEARCH",
         provider: "google",
-        query: urlParameters.get("q"),
+        query,
       });
       console.log("Message sent");
     }
   }
 });
+
+const fetchProducts = async (query) => {
+  console.log("query", query);
+  const url = "https://boxcart.shop/api/public/extension/get-products";
+  const localUrl = "http://localhost:3000/api/public/extension/get-products";
+  const response = await fetch(localUrl, {
+    method: "GET",
+  });
+
+  console.log("response", response);
+};
--- pages/api/public/extension/get-products.js
+++ pages/api/public/extension/get-products.js
@@ -1,18 +1,12 @@
 import prisma from "@/lib/prisma";
-import Cors from "micro-cors";
-
-const cors = Cors({
-  origin: "*", // Set to the appropriate origin(s) for better security
-  methods: ["GET"],
-});
 
 export default async function handler(req, res) {
   const { method } = req;
 
   if (method === "GET") {
     try {
-      // Fetch products using Prisma
       const products = await prisma.product.findMany({
+        take: 5,
         include: {
           account: {
             select: {
@@ -21,16 +15,12 @@ export default async function handler(req, res) {
           },
         },
       });
+      console.log("products", products)
 
-      // Respond with CORS headers
-      cors(req, res, () => {
-        res.status(200).json({ products });
-      });
+      res.status(200).send({ products });
     } catch (error) {
-      console.error("Error fetching products:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+      console.log(error);
+      res.status(500).send({ error });
     }
-  } else {
-    res.status(405).json({ error: "Method Not Allowed" });
   }
 }
