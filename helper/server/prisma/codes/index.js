918e2fdee9ffe1229df9240baf230ef4f4253940
Garwing <garwinglai@gmail.com>
2024-02-02 02:57:11 -0800

    imported prisma to access code api

--- helper/server/prisma/codes/index.js
+++ helper/server/prisma/codes/index.js
@@ -1,6 +1,6 @@
 // TODO: update code section.
 import { Role } from "@prisma/client";
-import { Prisma } from "@prisma/client";
+import prisma from "@/lib/prisma";
 
 export async function verifyAccessCode(code) {
   try {
